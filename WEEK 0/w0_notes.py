8354fd5620fb72f1418248449c95fa1fa2be45d8
anderson <133429641+popogis24@users.noreply.github.com>
2023-05-16 21:59:24 -0300

    Update w0_notes.py

--- WEEK 0/w0_notes.py
+++ WEEK 0/w0_notes.py
@@ -2,7 +2,6 @@
 15/05/2023 (30 minutes)
 Start: Introduction
 End: Multiple Function Arguments
-- input expects text, it's used only for string 
 '''
 #ask user for their name
 
@@ -12,3 +11,58 @@ x = input("what's your name? ")
 print (fr'hello, {x}')
 print ('hello, ' + x)
 print ('hello,', x)
+
+
+'''
+16/05/2023 (1 hour)
+Start: Named Parameters
+End: Type Conversion
+'''
+print(f'hello \n {x}') #\n steps one line
+print(f'hello, ',x, end="???")
+print ('how are you')
+print(f'hello, ', x, sep ='???')    
+print ("hello, 'friend'") 
+print (f"hello {x}")
+
+## now we gonna see how to format str##
+
+y = input ("Whats your name? ")
+y = y.strip() #remove whitespace from string
+y = y.capitalize() #capitalize the string
+y = y.title() #capitalize first letter of each word
+y = y.strip().title() #merge string methods
+print (f'hello, {y}')
+
+#the input can have all those config just by coding like that:
+
+z = input ("What's your name? ").strip().title()
+
+first, last= z.split() #split user's name into first name and last name
+
+print (f'hello, {first} the spider, {last}')
+
+#Now its the integer class, we gonna see how to use .int, (it's numbers without decimal points)
+
+#This one concatenate because you didn't informed the code that u want to convert the input x as a integer
+x = input ("What's x? ")
+y = input ("What's y? ")
+z = x + y
+print (z)
+
+#Here in the line 11 we convert the variables to integer by using int() function
+x = input ("What's x? ")
+y = input ("What's y? ")
+z = int(x) + int(y)
+print (z)
+
+#or you can inform that the input is going to be an int
+x = int(input ("What's x? "))
+y = int(input ("What's y? "))
+print (x+y)
+
+#or... 
+
+print(int(input("What's your x? "))+int(input("What's your y? ")))
+
+#Now we gonna see some similar as int, it's gonna be the FLOAT (the one that i usually use on arcmap)
