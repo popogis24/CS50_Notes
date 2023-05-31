1c2ccb2d0c2172f00a5624bafee2b6e84dec18b0
anderson <133429641+popogis24@users.noreply.github.com>
2023-05-30 21:44:25 -0300

    Update w2_notes.py

--- WEEK 2/w2_notes.py
+++ WEEK 2/w2_notes.py
@@ -1,9 +1,9 @@
 '''
 Start of the LOOP class
 
-28/05/2023 (xx minutes)
+28/05/2023 (1 hour)
 Start: Introduction
-End: 
+End: len
 '''
 
 #a manual loop for 3 meows
@@ -93,5 +93,82 @@ for student in students:
 
 students = ["Hermione", "Harry", "Ron"]
 
-for i in range(len(students)):
+for i in range(len(students)): #len show me how long is the list
     print(i+1, students[i]) #the i is bc i want to make a ranking starting by 1 
+
+
+##now we gonna learn about dictionaries or DICT
+
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco":"Slytherin"
+    }
+
+for student in students:
+    print(student)
+
+##now we gonna use the key to index 
+
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco":"Slytherin"
+    }
+
+for student in students:
+    print(student, students[student], sep=', ')
+
+##now we gonna make each student turns in to a dict
+
+students = [
+    {"name": "Hermione","house":"Gryffindor","patronus":"Otter"},
+    {"name": "Harry","house":"Gryffindor","patronus":"Stag"},
+    {"name": "Ron","house":"Gryffindor","patronus":"Jack Russel Terrier"},
+    {"name": "Draco","house":"Slytherin","patronus": None},    
+    ]
+for student in students:
+    print(student["name"], student["house"], student["patronus"], sep=", ")
+    
+#mario nested loop
+
+def main():
+    print_column(3)
+
+
+def print_column(height):
+    for _ in range(height):
+        print("#")
+
+main() 
+
+####
+
+def main():
+    print_row(4)
+
+def print_row(width):
+    print("?"*width)
+
+main()
+
+####
+
+def main():
+    print_square(3)
+
+def print_square(size):
+
+    #for each row in square
+    for i in range(size):
+
+        #for each brick in row
+        for j in range(size):
+
+            #print brick
+            print("# ", end="")
+        print()
+
+main()
