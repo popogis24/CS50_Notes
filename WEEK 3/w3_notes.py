0d3a4e8013ee11115397d8ce6973299bcb8b33c8
anderson <133429641+popogis24@users.noreply.github.com>
2023-06-05 21:55:49 -0300

    Update w3_notes.py

--- WEEK 3/w3_notes.py
+++ WEEK 3/w3_notes.py
@@ -1,8 +1,24 @@
-#
 '''
 Start of the EXCEPTION class
 
-05/06/2023 (xx hour)
+05/06/2023 (1/2 hour)
 Start: Introduction
-End: 
+End: else
 '''
+
+#SyntaxError is an error in the writing of your code. Now we gonna learn to use Try
+
+try:
+    x = int(input("What's the x? "))
+    print(f"x is {x}")
+except ValueError:
+    print("X is not an integer")
+
+#if you want to call an variable defined inside a try, you have to do this:
+try:
+    x = int(input("What's the x? "))
+except ValueError:
+    print("X is not an integer")
+else:
+print(f"x is {x}")
+
